f4822720ad3815a2d218e99ae12f625096b23481
UlaJiang <ulajiang0419@gmail.com>
2024-09-11 21:20:39 +1000

    Update database-migration-service.ts

--- cdk/typescript/database-migration-service/bin/database-migration-service.ts
+++ cdk/typescript/database-migration-service/bin/database-migration-service.ts
@@ -15,7 +15,6 @@ new DatabaseMigrationServiceStack(app, 'DatabaseMigrationServiceStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '743311230884', region: 'ap-southeast-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
