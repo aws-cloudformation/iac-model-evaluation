2218386f79ec61fe14eb50acbd01c6f906233f3a
Akhil Jain <88995214+jainakhi@users.noreply.github.com>
2024-09-14 01:47:44 +1000

    Add samples for OpenSearch Domain (#49)
    * IOT Analytics Sample with CDK Python
    
    * IOT Analytics Sample with CDK Typescript
    
    * IOT Analytics Sample with CloudFormation
    
    * IOT Core with CloudFormation
    
    * IOT Core sample with CDK Typescript
    
    * IOT core sample using CDK Python
    
    * Adding gitignore and npmignore to CDK Typescipt example
    
    * Open Search Domain sample using CDK Python
    
    * adding gitignore file for Opensearch Domain CDK Python
    
    * Open Search Domain sample using CDK Typescript
    
    * adding gitignore and npmignore files to opensearch CDK Typescript
    
    * Open search domain example in CloudFormation

---
+++ cdk/typescript/iot-core/test/iot-core.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IotCore from '../lib/iot-core-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iot-core-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IotCore.IotCoreStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
